d178baf43e183c93142f1e4b72457d92959dd227
Нелли <nelly.91@mail.ru>
2022-12-08 22:20:23 +0300

    My first commit

---
+++ src/ru/skypro/homework/homework1.java
@@ -0,0 +1,46 @@
+package ru.skypro.homework;
+
+public class homework1 {
+    public static void main(String[] args) {
+
+
+        // первое задание
+        byte a = 1;
+        short b = 100;
+        int c = 5000;
+        long d = 177L;
+        float f = 1.5f;
+        double e = 2.356;
+        boolean word = true;
+        char g = 12;
+
+        // второе задание
+        float sportsman1 = 78.2f;
+        float sportsman2 = 82.7f;
+        float generalWeight = sportsman1 + sportsman2;
+        float differenceWeight = sportsman2 - sportsman1;
+        System.out.println("Общий вес спортсменов " + generalWeight + " кг ");
+        System.out.println("Один спортсмен тяжелее другого на " + differenceWeight + " кг ");
+        System.out.println();
+
+        // третье задание
+        int banana = 80;
+        int milk = 105;
+        int iceCream = 100;
+        int egg = 70;
+        int recipe = banana * 5 + milk * 2 + iceCream * 2 + egg * 4;
+        int recipe1 = recipe / 1000;
+        System.out.println("Общий вес завтрака " + recipe1 + "кг");
+        System.out.println();
+
+
+        // четвертое задание
+
+
+
+    }
+
+
+}
+
+
