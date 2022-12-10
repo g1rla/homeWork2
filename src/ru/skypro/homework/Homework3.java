17f028bbd705067a1db851ccc56ceaa47bafb639
Нелли <nelly.91@mail.ru>
2022-12-10 20:42:45 +0300

    My second commit

---
+++ src/ru/skypro/homework/Homework3.java
@@ -0,0 +1,17 @@
+package ru.skypro.homework;
+
+public class Homework3 {
+    public static void main(String[] args) {
+        // первое задание
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i);
+        }
+        System.out.println();
+        int a = 11;
+        while (a > 1) {
+            a--;
+            System.out.print(a);
+        }
+    }
+
+}
